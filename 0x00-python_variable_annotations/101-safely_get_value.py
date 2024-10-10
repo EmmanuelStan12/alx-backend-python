e0854e7eaaf29a53a810e02f6600cad2d6e82470
Emmanuel Stanley <emmanuelstanley753@gmail.com>
2024-10-10 18:15:21 +0100

    task: more involved mappigns

---
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+More involved type annotations
+"""
+from typing import TypeVar, Mapping, Any, Union
+
+
+T = TypeVar('T')
+
+
+def safely_get_value(dct: Mapping, key: Any, default: Union[T, None] = None) -> Union[T, None]:
+    """
+    Generics in python annotations
+    """
+    if key in dct:
+        return dct[key]
+    else:
+        return default
