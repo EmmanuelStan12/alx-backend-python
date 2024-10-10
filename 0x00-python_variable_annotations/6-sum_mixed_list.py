0c47bc4a893f9f3fbb074934488b2a7bf61ce262
Emmanuel Stanley <emmanuelstanley753@gmail.com>
2024-10-10 17:46:10 +0100

    task: Sum mixed list

---
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+Complex types - mixed lists
+"""
+from typing import List, Union
+
+
+def sum_mixed_list(mxd_list: List[Union[int, float]]): float:
+    """
+    Returns the sum of mxd_list
+    """
+    return float(sum(mxd_list))
