86296a786a7b4193f44f48bb9598e1e24f64cf25
Emmanuel Stanley <emmanuelstanley753@gmail.com>
2024-10-10 17:28:12 +0100

    Basic annotations - add

---
+++ 0-add.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+Add floats together with type annotations
+"""
+
+
+def add(a: float, b: float) -> float:
+    """
+    Adds two floats and returns a float
+    """
+    return a + b
