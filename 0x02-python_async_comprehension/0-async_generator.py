fc80ede948a1bf9b772c839e061042eedbc25e6c
Emmanuel Stanley <emmanuelstanley753@gmail.com>
2024-10-15 20:54:31 +0100

    task: Async comprehension

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -3,9 +3,10 @@
 """
 import asyncio
 import random
+from typing import Generator
 
 
-async def async_generator():
+async def async_generator() -> Generator[float, None, None]:
     """This generator yields a number every
     1 second
     """
