c1d79aeef346bce3cdeee08577b975a404358be5
Emmanuel Stanley <emmanuelstanley753@gmail.com>
2024-10-15 20:57:58 +0100

    task: Async comprehension

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 """Task 1: Async comprehensions
 """
-import asyncio
 from typing import List
 
 
@@ -11,7 +10,4 @@ async_generator = __import__('0-async_generator').async_generator
 async def async_comprehension() -> List[float]:
     """Collect and return the numbers
     """
-    result = []
-    async for i in async_generator():
-        result.append(i)
-    return result
+    return [i async for i in async_generator()]
